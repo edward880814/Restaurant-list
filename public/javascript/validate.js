fd49a72f6b4d675882bd788c26f1263693828422
frank <frankcool321@gmail.com>
2022-12-15 13:04:57 +0800

    refactor: modify validate.js

--- public/javascript/validate.js
+++ public/javascript/validate.js
@@ -1,10 +1,12 @@
 const form = document.querySelector("#new-form");
-const submitButton = document.querySelector("#submitButton");
-form.addEventListener("submit", function onFormSubmitted(event) {
-  //- 若表單驗證未通過，則阻止傳送表單
-  if (!form.checkValidity()) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-  form.classList.add("was-validated");
-});
+//- 若在新增/編輯頁面才新增監聽
+if (form) {
+  form.addEventListener("submit", function onFormSubmitted(event) {
+    //- 若表單驗證未通過，則阻止傳送表單
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add("was-validated");
+  });
+}
