f98423659c7833cbfef0810eca2a1db280cdbe82
edward880814 <edward880814@gmail.com>
2023-04-27 09:01:26 +0800

    feat: add user.js to define data structure of every user

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+//定義User的資料結構
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
