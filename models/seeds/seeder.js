be8784810f563ceb92bdc34beec4833fcadf8d98
edward880814 <edward880814@gmail.com>
2023-04-28 14:52:16 +0800

    feat: modify seeder

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,25 +1,50 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 //- 引入Restaurant model
 const Restaurant = require("../restaurant");//..代表相對路徑中指向上一層目錄
-//- require resstaurant json file
-const restaurantSeed = require("../../restaurant.json").results;
-//! connect to db
+const User = require('../user')
 const db = require("../../config/mongoose")
+//- require restaurant json file
+const restaurantSeed = require("../../restaurant.json")
+const restaurants = restaurantSeed.results
+
+const SEED_USER = {
+  user1: {
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantId: [1, 2, 3]
+  },
+  user2: {
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantId: [4, 5, 6]
+  }
+}
+
 db.once("open", () => {
-  //- 連線後加入種子資料
-  // restaurantSeed.forEach((restaurant) => {
-  //   Restaurant.create({
-  //     name: restaurant.name,
-  //     name_en: restaurant.name_en,
-  //     category: restaurant.category,
-  //     image: restaurant.image,
-  //     location: restaurant.location,
-  //     phone: restaurant.phone,
-  //     google_map: restaurant.google_map,
-  //     rating: restaurant.rating,
-  //     description: restaurant.description,
-  //   });
-  // });
-  
-  Restaurant.insertMany(restaurantSeed);
-  console.log("Seed data added successfully!!");
-});
+  for (let key in SEED_USER) {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER[key].password, salt))
+      .then(hash => User.create({
+        email: SEED_USER[key].email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        let userRestaurant = restaurants.filter(restaurants => SEED_USER[key].restaurantId.includes(restaurants.id))
+        return Promise.all(Array.from(
+          { length: userRestaurant.length },
+          (_, i) => {
+            let { name, name_en, category, image, location, phone, google_map, rating, description } = userRestaurant[i]
+            Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
+          }))
+      })
+      .then(() => {
+        console.log(`${key} DB done`)
+      })
+  }
+})
